fe82dd1d2578a3868ae285b951c57c584ea34832
SoothTheRat <xblashxxzapata@gmail.com>
2024-01-12 17:12:28 -0300

    cambio color h5

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log('Bryan Test')
+
+function colorh5a() {
+    let colors = ["red", "blue", "green"];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById("colorh5a").style.color = randomColor;
+  }
+  function colorh5b() {
+    let colors = ["red", "blue", "green"];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById("colorh5b").style.color = randomColor;
+  }
+  function colorh5c() {
+    let colors = ["red", "blue", "green"];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById("colorh5c").style.color = randomColor;
+  }
+
+  function colorh5d() {
+    let colors = ["red", "blue", "green"];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById("colorh5d").style.color = randomColor;
+  }
