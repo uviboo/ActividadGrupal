195c14dd88ab995c231e8811eaadd56b0c132d07
Bryan Poblete <bryanpobletereyes@gmail.com>
2024-01-12 17:30:53 -0300

    Funcion para cambiar aleatoriamente el fondo

--- script.js
+++ script.js
@@ -1,1 +1,121 @@
-console.log('Bryan Test')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const bodyElement = document.getElementById('body-id');
+
+randomColors();
+
+setInterval(randomColors, 10000);
+
+    function randomColors() {
+        let numeroRandom = Math.floor(Math.random() * 3 ) + 1
+
+        if (numeroRandom === 1) {
+            bodyElement.style.backgroundColor = 'green';
+        } else if (numeroRandom === 2) {
+            bodyElement.style.backgroundColor = 'blue';
+        } else {
+            bodyElement.style.backgroundColor = 'red';
+        }
+    }
+
+
+
+
+
+
+
