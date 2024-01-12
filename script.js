357f77356b701697a6b08329dfc51bc7091deb69
Bryan Poblete <bryanpobletereyes@gmail.com>
2024-01-12 18:01:21 -0300

    Actualizando JS

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 
@@ -106,42 +105,10 @@ setInterval(randomColors, 10000);
         let numeroRandom = Math.floor(Math.random() * 3 ) + 1
 
         if (numeroRandom === 1) {
-            bodyElement.style.backgroundColor = 'green';
+            bodyElement.style.background-color = 'green';
         } else if (numeroRandom === 2) {
-            bodyElement.style.backgroundColor = 'blue';
+            bodyElement.style.background-color = 'blue';
         } else {
-            bodyElement.style.backgroundColor = 'red';
+            bodyElement.style.background-color = 'red';
         }
     }
-
-
-
-
-
-
-
-=======
-console.log('Bryan Test')
-
-function colorh5a() {
-    let colors = ["red", "blue", "green"];
-    let randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.getElementById("colorh5a").style.color = randomColor;
-  }
-  function colorh5b() {
-    let colors = ["red", "blue", "green"];
-    let randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.getElementById("colorh5b").style.color = randomColor;
-  }
-  function colorh5c() {
-    let colors = ["red", "blue", "green"];
-    let randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.getElementById("colorh5c").style.color = randomColor;
-  }
-
-  function colorh5d() {
-    let colors = ["red", "blue", "green"];
-    let randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.getElementById("colorh5d").style.color = randomColor;
-  }
->>>>>>> 9dfd4795df7d3ed15b2bac8820ade5627a8d92ab
