2c8bcc3c51c723dd69c831696edd0e5d00769c8c
Bryan Poblete <bryanpobletereyes@gmail.com>
2024-01-12 18:06:57 -0300

    Troubleshooting Javascript

--- script.js
+++ script.js
@@ -1,99 +1,28 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function colorh5a() {
+    let colors = ["red", "blue", "green"];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById("colorh5a").style.color = randomColor;
+  }
+  function colorh5b() {
+    let colors = ["red", "blue", "green"];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById("colorh5b").style.color = randomColor;
+  }
+  function colorh5c() {
+    let colors = ["red", "blue", "green"];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById("colorh5c").style.color = randomColor;
+  }
+
+  function colorh5d() {
+    let colors = ["red", "blue", "green"];
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById("colorh5d").style.color = randomColor;
+  }
+
+
+
+// Cambiar color del fondo aleatoriamente
 
 const bodyElement = document.getElementById('body-id');
 
@@ -101,14 +30,14 @@ randomColors();
 
 setInterval(randomColors, 10000);
 
-    function randomColors() {
-        let numeroRandom = Math.floor(Math.random() * 3 ) + 1
+function randomColors() {
+    let numeroRandom = Math.floor(Math.random() * 3 ) + 1
 
-        if (numeroRandom === 1) {
-            bodyElement.style.background-color = 'green';
-        } else if (numeroRandom === 2) {
-            bodyElement.style.background-color = 'blue';
-        } else {
-            bodyElement.style.background-color = 'red';
-        }
+    if (numeroRandom === 1) {
+        bodyElement.style.backgroundColor = 'green';
+    } else if (numeroRandom === 2) {
+        bodyElement.style.backgroundColor = 'blue';
+    } else {
+        bodyElement.style.backgroundColor = 'red';
     }
+}
